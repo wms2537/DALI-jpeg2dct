8cf06c2a2d49cd7e408632bddfd5a42eb7ea15ac
wms2537 <swmeng@yes.my>
2021-04-08 21:21:47 +0800

    fix type bugs

--- jpeg2dct.cc
+++ jpeg2dct.cc
@@ -24,43 +24,27 @@ void JpegToDct::RunImpl(::dali::SampleWorkspace &ws) {
   } catch (std::runtime_error &e) {
     DALI_FAIL(e.what() + ". File: " + file_name);
   }
-  ::dali::Image::Shape dct_y_shape = {bands[0].dct_h, bands[0].dct_w, bands[0].dct_b};
-  dct_y.Resize(dct_y_shape, ::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
+  dct_y.set_type(::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
+  dct_y.Resize({bands[0].dct_h, bands[0].dct_w, bands[0].dct_b}, ::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
   dct_y.SetLayout("HWC");
-  unsigned char *out_data = dct_y.mutable_data<unsigned char>();
-  std::memcpy(out_data, (void *)(bands[0].dct), ::dali::volume(dct_y_shape));
+  std::memcpy(dct_y.mutable_data<short>(), bands[0].dct, sizeof(short) * bands[2].dct_h * bands[2].dct_w * bands[2].dct_b);
   delete[] bands[0].dct;
 
   if(channels_ > 1) {
-    ::dali::Image::Shape dct_cb_shape = {bands[1].dct_h, bands[1].dct_w, bands[1].dct_b};
-    dct_cb.Resize(dct_cb_shape, ::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
+    dct_cb.set_type(::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
+    dct_cb.Resize({bands[1].dct_h, bands[1].dct_w, bands[1].dct_b}, ::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
     dct_cb.SetLayout("HWC");
-    unsigned char *out_data = dct_cb.mutable_data<unsigned char>();
-    std::memcpy(out_data, (void *)(bands[1].dct), ::dali::volume(dct_cb_shape));
+    std::memcpy(dct_cb.mutable_data<short>(), bands[1].dct, sizeof(short) * bands[1].dct_h * bands[1].dct_w * bands[1].dct_b);
     delete[] bands[1].dct;
   }
 
   if(channels_ > 2) {
-    ::dali::Image::Shape dct_cr_shape = {bands[2].dct_h, bands[2].dct_w, bands[2].dct_b};
-    dct_cr.Resize(dct_cr_shape, ::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
+    dct_cr.set_type(::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
+    dct_cr.Resize({bands[2].dct_h, bands[2].dct_w, bands[2].dct_b}, ::dali::TypeTable::GetTypeInfo(::dali::DALIDataType::DALI_INT16));
     dct_cr.SetLayout("HWC");
-    unsigned char *out_data = dct_cr.mutable_data<unsigned char>();
-    std::memcpy(out_data, (void *)(bands[2].dct), ::dali::volume(dct_cr_shape));
+    std::memcpy(dct_cr.mutable_data<short>(), bands[2].dct, sizeof(short) * bands[2].dct_h * bands[2].dct_w * bands[2].dct_b);
     delete[] bands[2].dct;
   }
-  // ::dali::TypeInfo type = input.type();
-  // auto &tp = ws.GetThreadPool();
-  // const auto &in_shape = input.shape();
-  // for (int sample_id = 0; sample_id < in_shape.num_samples(); sample_id++) {
-  //   tp.AddWork(
-  //       [&, sample_id](int thread_id) {
-  //         type.Copy<::dali::CPUBackend, ::dali::CPUBackend>(output.raw_mutable_tensor(sample_id),
-  //                                                           input.raw_tensor(sample_id),
-  //                                                           in_shape.tensor_size(sample_id), 0);
-  //       },
-  //       in_shape.tensor_size(sample_id));
-  // }
-  // tp.RunAll();
 }
 
 }  // namespace jpeg2dct
